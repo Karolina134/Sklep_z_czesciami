a8c96749407c9ac0a6d2c4a57b24740d393ed573
Karolina134 <117594414+Karolina134@users.noreply.github.com>
2022-11-13 16:31:32 +0100

    Initial commit

---
+++ src/main/java/pl/slowacki/Sklep_z_czesciami/SklepZCzesciamiApplication.java
@@ -0,0 +1,13 @@
+package pl.slowacki.Sklep_z_czesciami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepZCzesciamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepZCzesciamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/slowacki/Sklep_z_czesciami/SklepZCzesciamiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.slowacki.Sklep_z_czesciami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SklepZCzesciamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
